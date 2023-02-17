98c9d0ede68b46b8348892101d19dcca47d03679
Mayank <mayankdhankar2002@gmail.com>
2023-02-18 02:25:06 +0530

    Local Settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -127,3 +127,8 @@ MEDIA_URL = 'media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("No local settings found")
