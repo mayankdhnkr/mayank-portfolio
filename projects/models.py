0790a935ca2711ac571c646742c311ab7fdc3971
Mayank <mayankdhankar2002@gmail.com>
2023-06-09 00:00:18 +0530

    github and live both link added

--- projects/models.py
+++ projects/models.py
@@ -6,7 +6,8 @@ class project(models.Model):
     title=models.CharField(max_length=100)
     description=models.CharField(max_length=250)
     image=models.ImageField(upload_to='projects/images/')
-    url=models.URLField(blank=True)
+    github_url=models.URLField(blank=True)
+    live_url=models.URLField(blank=True)
     
     WEBDEV="WD";
     VIDEOEDIT="VE";
